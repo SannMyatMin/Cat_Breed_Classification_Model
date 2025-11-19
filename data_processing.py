385a53df47b2498ab4d3a15646ac259b3c66e93e
SannMyatMin <sanmyatmin88@gmail.com>
2025-11-19 18:01:26 +0630

    data generation and visualization

--- data_processing.py
+++ data_processing.py
@@ -4,6 +4,7 @@ import pandas as pd
 import tensorflow as tf
 from pathlib import Path
 from tensorflow import keras
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 class CatDataProcessor:
@@ -80,11 +81,93 @@ class CatDataProcessor:
     
     def create_train_validation_test_split(self):
         train_val_df, test_df = train_test_split(
-            self.df, test_size=self.test_split, stratify=self.df['breed'], random_state=42)
+            self.df, test_size=self.test_split, stratify=self.df['breed'], random_state=42 )
         train_df, validation_df = train_test_split(
-            train_val_df, test_size=self.validation_split, stratify=train_val_df['breed'], random_state=42)
+            train_val_df, test_size=self.validation_split, stratify=train_val_df['breed'], random_state=42 )
         
         return train_df, validation_df, test_df
+    
+    def create_data_generator(self, train_df, val_df, test_df):
+        train_datagen = keras.preprocessing.image.ImageDataGenerator(
+            rescale=1./255,
+            rotation_range=20,
+            width_shift_range=0.2, 
+            height_shift_range=0.2,
+            horizontal_flip=True, 
+            zoom_range=0.2,
+            brightness_range=[0.8, 1.2],
+            shear_range=0.15,
+            fill_mode="nearest" )
+         
+        
+        val_test_datagen = keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
+
+        train_generator = train_datagen.flow_from_dataframe(
+            train_df,
+            x_col="img_path",
+            y_col="breed",
+            target_size=self.image_size,
+            batch_size=self.batch_size,
+            class_mode="categorical",
+            shuffle=True )
+        
+        validation_generator = val_test_datagen.flow_from_dataframe(
+            val_df,
+            x_col="img_path",
+            y_col="breed",
+            target_size=self.image_size,
+            batch_size=self.batch_size,
+            class_mode="categorical",
+            shuffle=False )
+        
+        test_generator = val_test_datagen.flow_from_dataframe(
+            test_df,
+            x_col="img_path",
+            y_col="breed",
+            target_size=self.image_size,
+            batch_size=self.batch_size,
+            class_mode="categorical",
+            shuffle=False )
+        
+        self.class_names = list(train_generator.class_indices.keys())
+        self.class_indices = train_generator.class_indices
+
+        return train_generator, validation_generator, test_generator
+    
+    def analyze_class_distribution(self, train_df, val_df, test_df):
+        fig,ax = plt.subplots(4, 1, figsize=(24, 37))
+
+        train_df_count = train_df['breed'].value_counts()
+        ax[0].bar(range(len(train_df_count)), train_df_count.values, width=0.8)
+        ax[0].set_title("Training Set", fontweight="bold", fontsize=24)
+        ax[0].set_ylabel("Count")
+        ax[0].set_xticks(range(len(train_df_count)))
+        ax[0].set_xticklabels(train_df_count.index, rotation=90, fontsize=15)
+
+        val_df_count = val_df['breed'].value_counts()
+        ax[1].bar(range(len(val_df_count)), val_df_count.values)
+        ax[1].set_title("Validation Set", fontweight="bold", fontsize=24)
+        ax[1].set_ylabel("Count")
+        ax[1].set_xticks(range(len(val_df_count)))
+        ax[1].set_xticklabels(val_df_count.index, rotation=90, fontsize=15)
+
+        test_df_count = test_df['breed'].value_counts()
+        ax[2].bar(range(len(test_df_count)), test_df_count.values)
+        ax[2].set_title("Testing Set", fontweight="bold", fontsize=24)
+        ax[2].set_ylabel("Count")
+        ax[2].set_xticks(range(len(test_df_count)))
+        ax[2].set_xticklabels(test_df_count.index, rotation=90, fontsize=15)
+
+        ax[3].text(0.1, 0.8, "Class Distribution Status", fontsize=24, fontweight="bold")
+        ax[3].text(0.1, 0.7, f"Maximum Samples per Class = {max(train_df_count)}", fontsize=15)
+        ax[3].text(0.1, 0.6, f"Minimum Samples per Class = {min(train_df_count)}", fontsize=15)
+        ax[3].text(0.1, 0.5, f"Average Samples per Class = {train_df_count.mean():.2f}", fontsize=15)
+        ax[3].set_xlim(0,1)
+        ax[3].set_ylim(0,1)
+        ax[3].axis('off')
+
+        plt.tight_layout()
+        plt.show()
 
 
 
